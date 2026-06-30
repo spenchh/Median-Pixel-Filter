c3bdccb96e4b7c3c860569dc6dc27268aa0ea225
spenchh <270203916+spenchh@users.noreply.github.com>
2026-06-30 16:48:44 -0500

    Update pixel_pkg.sv

--- src/pixel_pkg.sv
+++ src/pixel_pkg.sv
@@ -1,1 +1,15 @@
+package pixel_pkg;
+  // Every color channel is 8 bits wide
+  localparam int PIXEL_W = 8;
+  /* 
+    Packed struct acts like a bundle of bits (Usually what you want for RTL/Synthesis)
+    For inputs / outputs
+  */
+  typedef struct packed {
+    logic [PIXEL_W-1:0] red;
+    logic [PIXEL_W-1:0] green;
+    logic [PIXEL_W-1:0] blue;
+  } pixel_t;
 
+endpackage
+    
